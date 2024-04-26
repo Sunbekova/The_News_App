d774476971599c36a8d744894871b77d0a355b28
Sunbekova <suanbekova05@gmail.com>
2024-04-26 07:21:37 +0500

    добавили Api и Retrofit

---
+++ app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.thenewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "ca8d87c391044b388d6514383074d22d"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}
