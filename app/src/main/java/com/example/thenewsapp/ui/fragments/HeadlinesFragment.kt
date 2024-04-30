1990c9d45f92ed4128e1538327a03518874f8db9
Sunbekova <suanbekova05@gmail.com>
2024-05-01 01:36:44 +0500

    dont work MainPage [notFullFragment]

--- app/src/main/java/com/example/thenewsapp/ui/fragments/HeadlinesFragment.kt
+++ app/src/main/java/com/example/thenewsapp/ui/fragments/HeadlinesFragment.kt
@@ -1,13 +1,113 @@
 package com.example.thenewsapp.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.provider.SyncStateContract.Constants
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.thenewsapp.R
+import com.example.thenewsapp.adapter.NewsAdapter
+import com.example.thenewsapp.databinding.FragmentHeadlinesBinding
+import com.example.thenewsapp.ui.NewsActivity
+import com.example.thenewsapp.ui.NewsViewModel
+import org.w3c.dom.Text
+import java.lang.Error
 
 
 class HeadlinesFragment : Fragment() {
 
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    lateinit var retryButton: Button
+    lateinit var errorText: TextView
+    lateinit var itemHeadlinesError: CardView
+
+
+    lateinit var binding: FragmentHeadlinesBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentHeadlinesBinding.bind(view)
+
+
+        itemHeadlinesError = view.findViewById(R.id.itemHeadlinesError)
+
+        val inflater = requireContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view: View = inflater.inflate(R.layout.item_error, null)
+
+        retryButton = view.findViewById(R.id.retryButton)
+
+        newsViewModel = (activity as NewsActivity).newsViewModel
+
+        setupHeadlinesRecycler()
+
+
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+
+            findNavController().navigate(R.id.action_headlinesFragment2_to_articleFragment, bundle)
+        }
+
+
+        retryButton.setOnClickListener {
+            newsViewModel.getHeadlines("ru")
+        }
+    }
+
+    var isScrolling = false
+
+
+    val scrollLisener = object : RecyclerView.OnScrollListener(){
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+            val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+            val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+            val visibleItemCount = layoutManager.childCount
+            val totalItemCount = layoutManager.itemCount
+
+            val isAtLastItem = firstVisibleItemPosition +visibleItemCount >= totalItemCount
+            val isNotAtBeginning = firstVisibleItemPosition >= 0
+            val isTotalMoreThanVisible = totalItemCount >= com.example.thenewsapp.util.Constants.QUERY_PAGE_SIZE
+            val shouldPaginate = isAtLastItem && isNotAtBeginning && isTotalMoreThanVisible && isScrolling
+
+            if (shouldPaginate){
+                newsViewModel.getHeadlines("ru")
+                isScrolling = false
+            }
+        }
+
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+
+            if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+                isScrolling = true
+            }
+        }
+
+    }
+
+
+    private fun setupHeadlinesRecycler(){
+        newsAdapter = NewsAdapter()
+        binding.recyclerHeadlines.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+            addOnScrollListener(this@HeadlinesFragment.scrollLisener)
+        }
+    }
 }
--- app/src/main/java/com/example/thenewsapp/util/Constants.kt
+++ app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object{
         const val API_KEY = "ca8d87c391044b388d6514383074d22d"
         const val BASE_URL = "https://newsapi.org/"
-        const val SEARCH_NEWS_TIME_DELAY = "500L"
-        const val QUERY_PAGE_SIZE = "20"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
